403e6accfc242a7a20439bbab4432129b6b7ace8
nathcarnevalli <nathaliacarnevalli004@gmail.com>
2023-05-10 16:50:21 -0300

    feat: create color mode functions (paintMode, pencilmode, eraserMode), getPalleteColor, getSliderSize and clearSketch function

--- js/index.js
+++ js/index.js
@@ -1,8 +1,45 @@
-function createGrid(n, m) {
+const DEFAULT_COLOR = '#ff0000'
+const DEFAULT_SIZE = 16
+
+let currentPaletteColor = DEFAULT_COLOR
+let currentSize = DEFAULT_SIZE
+
+function getPaletteColor(e) {
+  currentPaletteColor = e.target.value
+}
+
+function paintMode() {
+  currentPaletteColor = document.querySelector('.color-picker').value
+}
+
+function pencilMode() {
+  currentPaletteColor = 'black'
+}
+
+function eraserMode() {
+  currentPaletteColor = 'white'
+}
+
+function draw(e) {
+  const square = e.target
+  square.style.backgroundColor = currentPaletteColor
+}
+
+function clearSketch() {
+  window.location.reload()
+}
+
+function getSliderSize(e) {
+  const currentSize = e.target.value
+  const sliderSize = document.getElementById('sketchSize')
+  sliderSize.innerText = `${currentSize} x ${currentSize}`
+}
+
+function createGrid(n) {
   const container = document.querySelector('.container')
 
   for (let i = 0; i < n; i++) {
-    for (let j = 0; j < m; j++) {
+    for (let j = 0; j < n; j++) {
       const square = document.createElement('div')
       square.className = 'square'
       container.appendChild(square)
@@ -12,13 +49,4 @@ function createGrid(n, m) {
   }
 }
 
-function draw(e) {
-  const square = e.target
-  square.style.backgroundColor = 'blue'
-}
-
-function eraseAll() {
-  window.location.reload()
-}
-
-createGrid(16, 16)
+createGrid(currentSize)
