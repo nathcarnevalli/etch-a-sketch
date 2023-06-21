cdc9cacda2ed65fdfc79d6fbddb107d6b4a18ef7
nathcarnevalli <nathaliacarnevalli004@gmail.com>
2023-06-21 19:17:34 -0300

    feat: add grid size options, clear grid function, draw by clicking and image credits

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ const DEFAULT_SIZE = 16
 
 let currentPaletteColor = DEFAULT_COLOR
 let currentSize = DEFAULT_SIZE
+let drawSquare = false
 
 function getPaletteColor(e) {
   currentPaletteColor = e.target.value
@@ -21,8 +22,14 @@ function eraserMode() {
 }
 
 function draw(e) {
-  const square = e.target
-  square.style.backgroundColor = currentPaletteColor
+  if (drawSquare) {
+    const square = e.target
+    square.style.backgroundColor = currentPaletteColor
+  }
+}
+
+function canDraw() {
+  return drawSquare ? (drawSquare = false) : (drawSquare = true)
 }
 
 function clearSketch() {
@@ -33,20 +40,31 @@ function getSliderSize(e) {
   const currentSize = e.target.value
   const sliderSize = document.getElementById('sketchSize')
   sliderSize.innerText = `${currentSize} x ${currentSize}`
+  clearGrid()
+  createGrid(currentSize)
 }
 
 function createGrid(n) {
   const container = document.querySelector('.container')
 
+  container.style.gridTemplateColumns = `repeat(${n},  minmax(0, 1fr))`
+  container.style.gridTemplateRows = `repeat(${n}, minmax(0, 1fr)`
+
   for (let i = 0; i < n; i++) {
     for (let j = 0; j < n; j++) {
       const square = document.createElement('div')
+
       square.className = 'square'
       container.appendChild(square)
-
-      square.addEventListener('click', draw)
+      square.addEventListener('click', canDraw)
+      square.addEventListener('mouseover', draw)
     }
   }
+}
+
+function clearGrid() {
+  const container = document.querySelector('.container')
+  container.innerHTML = ''
 }
 
 createGrid(currentSize)
